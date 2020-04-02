e3d032a7e831398ec4552d8e7b1adc64e1ad6170
pete <petersyjuco@gmail.com>
2020-04-02 21:52:06 +0800

    Adds React.memo to prevent unnecessary rendering

--- chapter6/src/post/Post.js
+++ chapter6/src/post/Post.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { ThemeContext } from '../contexts'
 
 const Post = ({ title, content, author }) => {
+  console.log('rendering Post')
   const { secondaryColor } = useContext(ThemeContext)
 
   return (
@@ -14,4 +15,15 @@ const Post = ({ title, content, author }) => {
   )
 }
 
-export default Post
+export default React.memo(Post)
+
+/*
+With class components we had shouldComponentUpdate, which would prevent
+components from re-rendering if the props did not change.
+
+With function components, we can do the same using React.memo,
+which is a higher-order component. React.memo memoizes the result,
+which means that it will remember the last rendered result, and, in
+cases where the props did not change, it will skip re-rendering the
+component
+*/
