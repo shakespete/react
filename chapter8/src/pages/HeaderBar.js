3c4690cd840d2ca40917c5d159303c36f8325527
pete <petersyjuco@gmail.com>
2020-04-07 22:10:13 +0800

    Adds responsive ui

--- chapter8/src/pages/HeaderBar.js
+++ chapter8/src/pages/HeaderBar.js
@@ -3,23 +3,27 @@ import CreatePost from '../post/CreatePost'
 import UserBar from '../user/UserBar'
 import Header from '../Header'
 import ChangeTheme from '../ChangeTheme'
+import useWindowSize from '@rehooks/window-size'
 
 import { ThemeContext, StateContext } from '../contexts'
 
 export default function HeaderBar ({ setTheme }) {
   const theme = useContext(ThemeContext)
   const { state } = useContext(StateContext)
+  const { innerWidth } = useWindowSize()
+  const mobilePhone = innerWidth < 640
+
   const { user } = state
 
   return (
     <div>
       <Header text="React Hooks" />
-      <ChangeTheme theme={theme} setTheme={setTheme} />
-      <br />
-      <React.Suspense fallback={"Loading..."}>
-        <UserBar />
-      </React.Suspense>
-      <br />
+      {!mobilePhone && <ChangeTheme theme={theme} setTheme={setTheme} />}
+      {!mobilePhone && <br />}
+      {!mobilePhone && <React.Suspense fallback={"Loading..."}>
+          <UserBar />
+      </React.Suspense>}
+      {!mobilePhone && <br />}
       {user && <CreatePost /> }
     </div>
   )
