1d2c16fa36b9084d59416f221040ad207c131593
pete <petersyjuco@gmail.com>
2020-04-11 20:00:17 +0800

    Applies createSelector

--- chapter12/src/components/TodoList.js
+++ chapter12/src/components/TodoList.js
@@ -1,23 +1,31 @@
 import React, { useMemo } from 'react'
 import TodoItem from './TodoItem'
 import { useSelector } from 'react-redux'
+import { createSelector } from 'reselect'
 
-export default function TodoList () {
-  const filter = useSelector(state => state.filter)
-  const todos = useSelector(state => state.todos)
-  const filteredTodos = useMemo(() => {
+const todosSelector = state => state.todos
+const filterSelector = state => state.filter
+
+const selectFilteredTodos = createSelector(
+  todosSelector,
+  filterSelector,
+  (todos, filter) => {
     switch (filter) {
       case 'active':
         return todos.filter(t => t.completed === false)
-
+      
       case 'completed':
         return todos.filter(t => t.completed === true)
-
+      
       default:
       case 'all':
         return todos
     }
-  }, [ filter, todos ])
+  }
+)
+
+export default function TodoList () {
+  const filteredTodos = useSelector(selectFilteredTodos)
   
   return filteredTodos.map(item =>
     <TodoItem {...item} key={item.id} />
