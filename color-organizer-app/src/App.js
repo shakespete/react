a701b14119152733b132b232c0d72aa55db2a1d2
Jose Mari Syjuco <petersyjuco@tiw.local>
2020-05-24 16:17:54 +0800

    Inits color-org-app

---
+++ color-organizer-app/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import StarRating from "./components/StarRating";
+
+function App() {
+  return <StarRating />;
+}
+
+export default App;
---
+++ color-organizer-app/src/components/StarRating.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { FaStar } from "react-icons/fa";
+
+const Star = ({ selected = false }) => (
+  <FaStar color={selected ? "red" : "gray"} />
+);
+Star.propTypes = {
+  selected: PropTypes.bool.isRequired,
+};
+
+const createArray = (length) => [...Array(length)];
+
+const StarRating = ({ totalStars = 5 }) => {
+  const [selectedStars, setSelectedStars] = useState(3);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star key={i} selected={selectedStars > i} />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
+};
+StarRating.propTypes = {
+  totalStars: PropTypes.number.isRequired,
+};
+
+export default StarRating;
