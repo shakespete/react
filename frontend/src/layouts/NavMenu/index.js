802dda9816cbb4723238b416076177ac87539bac
shakespete <petersyjuco@gmail.com>
2020-10-06 11:23:55 +1100

    Completes routing

--- frontend/src/layouts/NavMenu/index.js
+++ frontend/src/layouts/NavMenu/index.js
@@ -1,29 +1,30 @@
 import React, { useState } from 'react';
 import { Layout, Menu } from 'antd';
 import { Link, useHistory } from 'react-router-dom';
-import {
-  AreaChartOutlined,
-  HddOutlined,
-  ReconciliationOutlined,
-  TeamOutlined,
-} from '@ant-design/icons';
+import routeList, { subMenuList } from 'routes/list';
 
 const { Sider } = Layout;
 const { SubMenu } = Menu;
 
+const getSelectedMenu = history => {
+  const urlPath = history.location.pathname;
+  const selMenu = routeList.find(route => route.path === urlPath);
+  return [selMenu.key, selMenu.parent];
+};
+
 const NavMenu = () => {
   let history = useHistory();
+  const [selected, menuParent] = getSelectedMenu(history);
+  const [openedMenu, setOpenedMenu] = useState([menuParent]);
+  const [selectedMenu, setSelectedMenu] = useState([selected]);
   const [collapsed, setCollapsed] = useState(false);
-
-  console.log(history); // Fix routing auto-select of menu
-  const [openedMenu, setOpenedMenu] = useState(['sub1']);
-  const [selectedMenu, setSelectedMenu] = useState(['1']);
 
   const openMenuHandler = openKeys => {
     setOpenedMenu(openKeys);
   };
 
-  const selectMenuHandler = ({ item, key, keyPath, domEvent }) => {
+  const selectMenuHandler = ({ item, key, keyPath, selectedKeys }) => {
+    // console.log(key);
     setSelectedMenu(key);
   };
 
@@ -38,37 +39,22 @@ const NavMenu = () => {
         openKeys={openedMenu}
         selectedKeys={selectedMenu}
         onOpenChange={openMenuHandler}
-        onClick={selectMenuHandler}
+        onSelect={selectMenuHandler}
       >
-        <SubMenu key="sub1" icon={<AreaChartOutlined />} title="Dashboards">
-          <Menu.Item key="1">
-            <Link to="/home">Home</Link>
-          </Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub2" icon={<TeamOutlined />} title="Admin">
-          <Menu.Item key="2">
-            <Link to="/management/sites">Sites</Link>
-          </Menu.Item>
-          <Menu.Item key="3">
-            <Link to="/management/tenants">Tenants</Link>
-          </Menu.Item>
-          <Menu.Item key="4">
-            <Link to="/management/users">Users</Link>
-          </Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub3" icon={<HddOutlined />} title="Assets and Locations">
-          <Menu.Item key="5">
-            <Link to="/assets">Manage Assets</Link>
-          </Menu.Item>
-          <Menu.Item key="6">
-            <Link to="/locations">Manage Locations</Link>
-          </Menu.Item>
-        </SubMenu>
-        <SubMenu key="sub4" icon={<ReconciliationOutlined />} title="Inventory">
-          <Menu.Item key="7">
-            <Link to="/brands">Manage Brands</Link>
-          </Menu.Item>
-        </SubMenu>
+        {subMenuList.map(submenu => (
+          <SubMenu key={submenu.key} icon={submenu.icon} title={submenu.title}>
+            {routeList.map(menu => {
+              if (menu.parent === submenu.key) {
+                return (
+                  <Menu.Item key={menu.key}>
+                    <Link to={menu.path}>{menu.name}</Link>
+                  </Menu.Item>
+                );
+              }
+              return null;
+            })}
+          </SubMenu>
+        ))}
       </Menu>
     </Sider>
   );
--- frontend/src/routes/list.js
+++ frontend/src/routes/list.js
@@ -1,9 +1,40 @@
-import { lazy } from 'react';
+import React, { lazy } from 'react';
+import {
+  AreaChartOutlined,
+  HddOutlined,
+  ReconciliationOutlined,
+  TeamOutlined,
+} from '@ant-design/icons';
+
+const subMenuList = [
+  {
+    key: 'dashboard',
+    title: 'Dashboards',
+    icon: <AreaChartOutlined />,
+  },
+  {
+    key: 'admin',
+    title: 'Admin',
+    icon: <TeamOutlined />,
+  },
+  {
+    key: 'assetsAndLocations',
+    title: 'Assets and Location',
+    icon: <HddOutlined />,
+  },
+  {
+    key: 'inventory',
+    title: 'Inventory',
+    icon: <ReconciliationOutlined />,
+  },
+];
 
 const routeList = [
   {
     name: 'Home',
     path: '/home',
+    key: 'home',
+    parent: 'dashboard',
     Component: lazy(() => import('pages/home')),
     exact: true,
     roles: [],
@@ -11,6 +42,8 @@ const routeList = [
   {
     name: 'Sites',
     path: '/management/sites',
+    key: 'sites',
+    parent: 'admin',
     Component: lazy(() => import('pages/management/sites')),
     exact: true,
     roles: [],
@@ -18,6 +51,8 @@ const routeList = [
   {
     name: 'Tenants',
     path: '/management/tenants',
+    key: 'tenants',
+    parent: 'admin',
     Component: lazy(() => import('pages/management/tenants')),
     exact: true,
     roles: ['get-tenant'],
@@ -25,6 +60,8 @@ const routeList = [
   {
     name: 'Users',
     path: '/management/users',
+    key: 'users',
+    parent: 'admin',
     Component: lazy(() => import('pages/management/users')),
     exact: true,
     roles: [],
@@ -32,6 +69,8 @@ const routeList = [
   {
     name: 'Manage Assets',
     path: '/assets',
+    key: 'assets',
+    parent: 'assetsAndLocations',
     Component: lazy(() => import('pages/assets')),
     exact: true,
     roles: ['get-asset'],
@@ -39,6 +78,8 @@ const routeList = [
   {
     name: 'Manage Locations',
     path: '/locations',
+    key: 'locations',
+    parent: 'assetsAndLocations',
     Component: lazy(() => import('pages/locations')),
     exact: true,
     roles: [],
@@ -46,10 +87,13 @@ const routeList = [
   {
     name: 'Manage Brands',
     path: '/brands',
+    key: 'brands',
+    parent: 'inventory',
     Component: lazy(() => import('pages/brands')),
     exact: true,
     roles: [],
   },
 ];
 
+export { subMenuList };
 export default routeList;
