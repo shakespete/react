b07d9305bb445df088dcfc6d571a51eda4632419
shakespete <petersyjuco@gmail.com>
2020-10-28 09:14:08 +1100

    05: Styling - Extra 2

---
+++ fundamentals/react-fundamentals/src/exercise/05.extra-2.js
@@ -0,0 +1,43 @@
+// Styling
+// http://localhost:3000/isolated/exercise/05.js
+
+import React from 'react'
+import '../box-styles.css'
+
+const Box = ({size, style, ...otherProps}) => {
+  return (
+    <div
+      className={`box box--${size}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
+
+const smallBox = (
+  <Box size="small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box size="medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box size="large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
+
+function App() {
+  return (
+    <div>
+      {smallBox}
+      {mediumBox}
+      {largeBox}
+    </div>
+  )
+}
+
+export default App
