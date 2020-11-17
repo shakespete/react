88c48a6eb74ed015249840ee28822b1982f8b36a
shakespete <petersyjuco@gmail.com>
2020-11-18 09:23:46 +1100

    02: Custom Hook

---
+++ fundamentals/react-hooks/src/exercise/02.extra-3.js
@@ -0,0 +1,36 @@
+// useEffect: persistent state
+// http://localhost:3000/isolated/exercise/02.js
+
+import * as React from 'react'
+
+const useLocalStorageState = (key, value = '') => {
+  const [local, setLocal] = React.useState(
+    () => window.localStorage.getItem(key) || value,
+  )
+  React.useEffect(() => {
+    window.localStorage.setItem(key, local)
+  }, [key, local])
+  return [local, setLocal]
+}
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useLocalStorageState('name', initialName)
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input value={name} onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  )
+}
+
+function App() {
+  return <Greeting />
+}
+
+export default App
