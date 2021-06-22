6bf3e6be1619a2ce74daebd454fb1d1ffad4ed44
shakespete <petersyjuco@gmail.com>
2021-06-23 09:38:17 +1000

    01: useReducer - simple Counter extra credit 4

---
+++ fundamentals/advanced-react-hooks/src/exercise/01.extra-4.js
@@ -0,0 +1,33 @@
+// useReducer: simple Counter
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+
+const countReducer = (state, action) => {
+  console.log(state)
+  console.log(action)
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        ...state,
+        count: state.count + action.step,
+      }
+    default:
+      return state
+  }
+}
+
+function Counter({initialCount = 0, step = 1}) {
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
+  const increment = () => dispatch({type: 'INCREMENT', step})
+  return <button onClick={increment}>{count}</button>
+}
+
+function App() {
+  return <Counter />
+}
+
+export default App
