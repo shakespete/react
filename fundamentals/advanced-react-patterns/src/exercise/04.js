33dbd7b2f1a6b0fc7d8560319e0f7e008a47de89
shakespete <petersyjuco@gmail.com>
2021-09-21 19:16:27 +1000

    04: Prop Collections and Getters

--- fundamentals/advanced-react-patterns/src/exercise/04.js
+++ fundamentals/advanced-react-patterns/src/exercise/04.js
@@ -8,10 +8,10 @@ function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-  const togglerProps = {'aria-pressed': on, onClick: toggle}
   // 🐨 Add a property called `togglerProps`. It should be an object that has
   // `aria-pressed` and `onClick` properties.
   // 💰 {'aria-pressed': on, onClick: toggle}
+  const togglerProps = {'aria-pressed': on, onClick: toggle}
   return {on, togglerProps}
 }
 
